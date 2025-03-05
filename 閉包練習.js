615f5bbc35111216dea30c171962a9e4453ecdf4
Tingan <syeak123@gmail.com>
2025-03-05 22:48:11 +0800

    fix:closure understand

--- 閉包練習.js
+++ 閉包練習.js
@@ -14,4 +14,31 @@ function createCounter(){
     // 回傳變數之後在外定義一個變數封裝呼叫函式 使其函數裡的變數
     // 變成私有變數 讓外部能夠呼叫
     
+    function createArrayManager(){
+        let arr=[]
+        return{
+            addItem:function(a){
+                arr.push(a)
+                console.log(`add:${a}`);
+                
+            },
+            removeItem:function(){
     
+                const reomved=arr.pop()
+                console.log(`Remove:${reomved}`)
+            },
+            showItems:function(){
+                console.log(arr);
+                
+            }            
+            }
+        }
+    const aa=createArrayManager();
+    aa.addItem('tttt')
+    aa.addItem('ssssss')
+    aa.removeItem()
+    aa.showItems()
+
+    // 閉包就像是一個有鎖的盒子，裡面有三顆球（外層變數），
+    // 只有特定的三把鑰匙（addItem、removeItem、showItems）可以打開這個盒子，
+    // 這樣外面的人無法直接接觸到球，只有這三個方法能操作它。
